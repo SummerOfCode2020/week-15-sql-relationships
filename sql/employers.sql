a78ed24ebd0d0f5dc85f0864cf2e0cd78569165c
Julie Kearns <juliekearns@Julies-MacBook-Air.local>
2020-09-09 19:28:46 -0400

    Create sql tables

---
+++ sql/employers.sql
@@ -0,0 +1,13 @@
+-- id and name column
+-- id INT
+-- name VARCHAR
+-- PK to define primary key
+
+
+CREATE TABLE
+IF NOT EXISTS employers
+(
+  id AUTO_INCREMENT INT NOT NULL,
+  name VARCHAR(200),
+  PRIMARY KEY(id),
+)
