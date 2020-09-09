bc156ba225ae101293b0d86f677f76f911a26ece
Hollis <hollis@Holliss-MacBook-Pro.local>
2020-09-09 19:30:54 -0400

    Fixes auto format errors

--- sql/members.sql
+++ sql/members.sql
@@ -2,10 +2,8 @@ CREATE TABLE
 IF NOT EXISTS members
 (
   id AUTO_INCREMENT INT NOT NULL UNIQUE,
-  name VARCHAR
-(225) NOT NULL
+  name VARCHAR(225) NOT NULL
   employerId INT NOT NULL
-  FOREIGN KEY
-(employerId) REFERENCES employers
-(id)
+  PRIMARY KEY (id)
+  FOREIGN KEY(employerId) REFERENCES employers(id)
 )
