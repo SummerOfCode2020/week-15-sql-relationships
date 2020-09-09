7d6d341629a9e8e2c9f1687bd5c9a6a2a3740225
Erick Pacheco <linarespachecoerick@gmail.com>
2020-09-09 16:28:54 -0700

    Creates new table

---
+++ sql/employers.sql
@@ -0,0 +1,6 @@
+create table if not exists employers
+(
+    ID AUTO_INCREMENT INT not null,
+    `name` VARCHAR (250),
+    primary key (ID)
+)
---
+++ sql/members.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS members 
+(
+    id int AUTO_INCREMENT,
+    `name` VARCHAR(250),
+    `employer_id` INT NOT NULL
+    PRIMARY KEY (id)    
+)
