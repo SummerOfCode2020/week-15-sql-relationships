c570fde37517f434082a0ae7b7483db171f4c0f6
Hollis <hollis@Holliss-MacBook-Pro.local>
2020-09-09 19:29:33 -0400

    Completes class quiz 1

---
+++ sql/members.sql
@@ -0,0 +1,11 @@
+CREATE TABLE
+IF NOT EXISTS members
+(
+  id AUTO_INCREMENT INT NOT NULL UNIQUE,
+  name VARCHAR
+(225) NOT NULL
+  employerId INT NOT NULL
+  FOREIGN KEY
+(employerId) REFERENCES employers
+(id)
+)
