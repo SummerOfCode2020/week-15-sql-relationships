d08f93adab0fba3999a725acfe2b12607f4883be
jessicareynolds <reynolds.jessann@gmail.com>
2020-09-09 19:28:08 -0400

    Add sql tables

---
+++ sql/employers.sql
@@ -0,0 +1,12 @@
+-- id and name columns
+-- id INT
+-- name VARCHAR
+-- PK to define primary key
+
+CREATE TABLE
+IF NOT EXISTS employers
+(
+    id AUTO_INCREMENT INT NOT NULL,
+    name VARCHAR(225) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
