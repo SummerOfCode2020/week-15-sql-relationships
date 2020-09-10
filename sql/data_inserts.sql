ede2ec203e9b6a53cb520b0d73041b47a7df54e3
Nick Lavery <tuneboynick@gmail.com>
2020-09-10 19:23:08 -0400

    More repetition and memory work with MySQL

--- sql/data_inserts.sql
+++ sql/data_inserts.sql
@@ -12,13 +12,13 @@ SELECT *
 FROM employers;
 
 SELECT *
-FROM members; 
+FROM members;
 
 
 -- Always SPECIFY!!
 DELETE
 FROM employers
-WHERE name = 
+WHERE name =
 
 -- INSERT MULTIPLE ROWS
 INSERT
@@ -33,13 +33,60 @@ VALUES
 -- INSERT Members
 INSERT INTO
 members
-(member_id, name)
+    (member_id, name)
 VALUES
-(111, "Nick"),
-(111, "Anna");
+    (111, "Nick"),
+    (111, "Anna");
 
 -- Join
 SELECT employers.name, members.name
 FROM employers
-JOIN members
+    JOIN members
     ON employers.id = members.member_id
+
+-- WHERE stuff
+SELECT employers.name,
+    members.name
+FROM employers
+    JOIN members ON employers.id = members.member_id
+WHERE employers.name = 'MaxwellHealth';
+
+SELECT employers.name, employers.id,
+    members.name, members.id
+FROM employers
+    JOIN members ON employers.id = members.member_id
+-- WHERE employers.name = 'MaxwellHealth';
+
+
+-- Referencing specific member by members.id 
+SELECT employers.name,
+    employers.id,
+    members.name,
+    members.id
+FROM employers
+    JOIN members ON employers.id = members.member_id
+    WHERE members.id = '1'
+
+-- Uisng LIKE with % (return anything) 
+    SELECT employers.name,
+    employers.id,
+    members.name,
+    members.id
+FROM employers
+    JOIN members ON employers.id = members.member_id
+    WHERE members.name LIKE '%nn%'
+
+-- Adding more records
+    INSERT INTO members (member_id, name)
+VALUES (111, "George"),
+    (111, "Jessica"),
+    (111, "Peter");
+
+-- ORDER
+SELECT * FROM employers
+ORDER BY name;
+
+-- ODER
+SELECT * FROM members
+ORDER BY name;
+
