a5b70fb54582b82a858d07b035eef850626a77fc
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-09-09 20:36:26 -0400

    Modify syntax to work with SQLTools

--- sql/employers.sql
+++ sql/employers.sql
@@ -3,7 +3,7 @@
 -- name VARCHAR
 -- PK to define primary key
 CREATE TABLE IF NOT EXISTS employers (
-    id AUTO_INCREMENT INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR (225) NOT NULL UNIQUE,
     PRIMARY KEY (id)
 )
--- sql/members.sql
+++ sql/members.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS members (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR (200) NOT NULL,
-    employedID INT UNSIGNED,
+    employedID INT,
     PRIMARY KEY (id),
     FOREIGN KEY (employedID) REFERENCES employers (id)
 )
