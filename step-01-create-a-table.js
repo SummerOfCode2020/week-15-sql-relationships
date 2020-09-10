be8830fee5734a83db09610fef57a6097ab83c6b
Hollis <hollis@Holliss-MacBook-Pro.local>
2020-09-09 20:26:36 -0400

    Fixes issues

--- step-01-create-a-table.js
+++ step-01-create-a-table.js
@@ -1,4 +1,4 @@
-const { sequelize } = require('./sequelize/sequelize-common');
+const { sequelize } = require("./sequelize/sequelize-common");
 
 /**
 
@@ -14,8 +14,7 @@ const { sequelize } = require('./sequelize/sequelize-common');
     See async-await.md
  */
 (async () => {
-
-    /**
+  /**
     There are often use cases in which it easiest 
     to execute raw / already prepared SQL queries, we can 
     use the `sequelize.query` method for this
@@ -24,23 +23,22 @@ const { sequelize } = require('./sequelize/sequelize-common');
 
      */
 
-    // <https://dev.mysql.com/doc/refman/8.0/en/create-table.html>
-    await sequelize.query(`
+  // <https://dev.mysql.com/doc/refman/8.0/en/create-table.html>
+  await sequelize.query(`
         
     `);
 
-    /*
+  /*
         to provide feedback on the sql above we get all tables
 
         See <https://dev.mysql.com/doc/refman/8.0/en/show.html>
         and <https://dev.mysql.com/doc/refman/8.0/en/show-tables.html>
     */
-    const [results, metadata] = await sequelize.query(`
+  const [results, metadata] = await sequelize.query(`
         SHOW TABLES
     `);
 
-    console.log({results, metadata});
-
-})().catch(error => {
-    console.log({error});
+  console.log({ results, metadata });
+})().catch((error) => {
+  console.log({ error });
 });
