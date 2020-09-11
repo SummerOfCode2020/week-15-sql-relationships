aba6306c9199604ddfed7aa044c3113ce997f222
Sharon Lin <hsiang.lin@gmail.com>
2020-09-10 20:21:49 -0400

    Add not-for-git in gitignore correctly

---
+++ sequelize/not-for-git-settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mysqlUrl: "mysql://b3622655719d34:8baebaca@us-cdbr-east-02.cleardb.com/heroku_7a1fd9919463c89",
+    username: "b3622655719d34",
+    password: "8baebaca",
+    host: "heroku_7a1fd9919463c89",
+    port: 3306
+} 
